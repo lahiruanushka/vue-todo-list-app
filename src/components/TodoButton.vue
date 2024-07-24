830882288a8a74c220168d13efa7d06507dc0acf
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-24 11:50:35 +0530

    Create TodoButton.vue

---
+++ src/components/TodoButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    :class="['p-3 rounded-md transition-colors duration-300', buttonClass, {
+      'bg-blue-500 text-white hover:bg-blue-600': !buttonClass
+    }]"
+    @click="handleClick"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'TodoButton',
+  props: {
+    buttonClass: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    handleClick(event) {
+      this.$emit('click', event);
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
